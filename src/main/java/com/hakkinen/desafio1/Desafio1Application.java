912d938b2f5bc3cb59bd91f2f7a1eee5eded3bc2
Tawanna Hakkinen <thol_16@hotmail.com>
2023-06-21 14:25:22 -0300

    Fix Data Input

--- src/main/java/com/hakkinen/desafio1/Desafio1Application.java
+++ src/main/java/com/hakkinen/desafio1/Desafio1Application.java
@@ -23,34 +23,26 @@ public class Desafio1Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-        /*System.out.println("=========== EXEMPLO 1 ==============");
+		Locale.setDefault(Locale.US);
+
 		Order exemplo1 = new Order(1034, 150.00, 20.0);
 		double total1 = orderService.total(exemplo1);
 		System.out.println("Pedido código " + exemplo1.getCode());
 		System.out.printf("Valor total: R$ %.2f%n", total1);
 
-		System.out.println("=========== EXEMPLO 2 ==============");
+		System.out.println();
 		Order exemplo2 = new Order(2282, 800.00, 10.0);
 		double total2 = orderService.total(exemplo2);
 		System.out.println("Pedido código " + exemplo2.getCode());
 		System.out.printf("Valor total: R$ %.2f%n", total2);
 
-		System.out.println("=========== EXEMPLO 3 ==============");
+		System.out.println();
 		Order exemplo3 = new Order(1309, 95.90, 0.0);
 		double total3 = orderService.total(exemplo3);
 		System.out.println("Pedido código " + exemplo3.getCode());
-		System.out.printf("Valor total: R$ %.2f%n", total3);*/
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		int code = sc.nextInt();
-		double basic = sc.nextDouble();
-		double discount = sc.nextDouble();
+		System.out.printf("Valor total: R$ %.2f%n", total3);
 
-		Order order = new Order(code, basic, discount);
-		double total = orderService.total(order);
-		System.out.println("Pedido código " + order.getCode());
-		System.out.printf("Valor total: R$ %.2f%n", total);
 
-		sc.close();
+
 	}
 }
